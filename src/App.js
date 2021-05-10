80cc30e4bb8fa0c27d0c97735074b8697b893fd2
Andrew Krause <andrew.l.krause@gmail.com>
2021-05-09 23:36:08 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
 
-function App() {
+const App = () => {
+  const [counter, setCounter] = useState(0)
+  const [visibility, setVisibility] = useState(false)
+
+  const handleCounter = () => { 
+
+    
+    if(counter > 10) { 
+      setVisibility(true)
+    }
+
+    return setCounter(prevState => prevState + 1)
+  }
+
+  const resetCounter = () => { 
+    setCounter(0)
+    setVisibility(false)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={handleCounter}>Click me</button>
+      <p style={{color: "red"}}>Count: {counter}</p>
+      {visibility === true && <button onClick={resetCounter}>Reset Button</button>}
     </div>
-  );
+  )
 }
 
 export default App;
