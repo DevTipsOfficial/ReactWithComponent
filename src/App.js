73197b131dbd68e4b51872fbcaa6fd67017f6cb8
Andrew Krause <andrew.l.krause@gmail.com>
2021-06-11 23:03:18 -0500

    add useEffect

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 
 const App = () => {
   const [counter, setCounter] = useState(0)
   const [visibility, setVisibility] = useState(false)
 
-  const handleCounter = () => { 
+  useEffect(() => {
+    document.title = `Count: ${counter}`
+  })
 
+  const handleCounter = () => { 
     
     if(counter > 10) { 
       setVisibility(true)
@@ -23,7 +26,9 @@ const App = () => {
     <div>
       <button onClick={handleCounter}>Click me</button>
       <p style={{color: "red"}}>Count: {counter}</p>
-      {visibility === true && <button onClick={resetCounter}>Reset Button</button>}
+      {visibility === true && (
+        <button onClick={resetCounter}>Reset Button</button>
+      )}
     </div>
   )
 }
